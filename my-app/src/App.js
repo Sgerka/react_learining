f10a0078ea0f8432e9df250c646a132dc16d8f61
germansmolyar <german@crunchmind.com>
2020-09-23 00:09:59 +0300

    Adding router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 import logo from './logo.svg';
 import './App.scss';
 import Header from "./header-component/header";
@@ -9,6 +15,19 @@ function App() {
     return (
         <div className="App">
             <Header> </Header>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <h1>Home</h1>
+                    </Route>
+                    <Route path="/share">
+                        <h1>Share</h1>
+                    </Route>
+                    <Route path="/get">
+                        <h1>get</h1>
+                    </Route>
+                </Switch>
+            </Router>
             <Footer> </Footer>
         </div>
     );
